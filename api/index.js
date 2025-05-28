c9f3435c1a59a69672729952171bce9b08b81cb6
priyanshu1129 <priyanshujaj11@gmail.com>
2025-05-28 14:42:16 +0530

    after removing serverless function

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import { connectDB } from "../config/db.js";
+import errorHandler from "../middleware/errorHandler.js";
 import authRouter from "../routes/auth.js";
 import propertyRouter from "../routes/property.js";
 import userRouter from "../routes/user.js";
-import errorHandler from "../middleware/errorHandler.js";
-import serverless from "serverless-http";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 connectDB();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("API is working");
 });
 
@@ -23,4 +23,7 @@ app.use("/api/property", propertyRouter);
 
 app.use(errorHandler);
 
-export const handler = serverless(app);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+);
